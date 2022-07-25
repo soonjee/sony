4cb239303dbd9e4c894e3d9608bacb4b267ae593
seungji Choi <youinxxs@naver.com>
2022-07-26 03:10:56 +0900

    0726

--- js/sony.js
+++ js/sony.js
@@ -4,6 +4,12 @@ $(function () {
         arrows: false,
         autoplay: true,
         vertical: true,
+    });
 
+    $('.basic_slider').slick({
+        arrows: false,
+        autoplay: true,
+        speed: 2000,
+        fade:true,
     });
 })
