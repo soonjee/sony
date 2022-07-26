a9c3c80c3cfc5bc65422d58d82f57a6a1f0edfaa
seungji Choi <youinxxs@naver.com>
2022-07-27 03:55:57 +0900

    20220727

--- js/sony.js
+++ js/sony.js
@@ -18,4 +18,24 @@ $(function () {
         speed: 3000,
         fade: true,
     });
+
+    $('.left_slider').slick({
+        arrows: false,
+        fade: true,
+    });
+    
+    $('.right_slider').slick({
+        arrows: false,
+        slidesToShow: 2,
+        asNavFor: '.left_slider',
+    });
+    
+
+    $('.sonyStore .right .xi-arrow-left').on('click', function () {
+        $('.right_slider').slick('slickPrev')
+    });
+    $('.sonyStore .right .xi-arrow-right').on('click', function () {
+        $('.right_slider').slick('slickNext')
+    });
+
 })
