ee8d3791eb93a2ca6551ee5f8431d864cd1cc3fa
soonji <youinxxs@naver.com>
2022-07-27 17:46:22 +0900

    20220727

--- js/sony.js
+++ js/sony.js
@@ -23,13 +23,21 @@ $(function () {
         arrows: false,
         fade: true,
     });
-    
+
     $('.right_slider').slick({
         arrows: false,
         slidesToShow: 2,
+        slidesToScroll: 1,
         asNavFor: '.left_slider',
     });
-    
+
+    $('.right_slider').on('afterChange', function (e, s, c) {
+        let t = c % 4;
+        $('.right .bar span').css({
+            left: t * 25 + '%'
+        });
+    });
+
 
     $('.sonyStore .right .xi-arrow-left').on('click', function () {
         $('.right_slider').slick('slickPrev')
